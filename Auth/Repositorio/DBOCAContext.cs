d34ed152ba1b448681557d2b80dd09ba69004da9
Marrudt Durán Reyes <marrudt@gmail.com>
2019-04-03 16:40:45 -0500

    Sincronizacion 3

---
+++ Auth/Repositorio/DBOCAContext.cs
@@ -0,0 +1,38 @@
+﻿using Auth.Models;
+using System.Data.Entity;
+
+namespace Auth.Repositorio
+{
+    public class DBOCAContext : DbContext
+    {
+        public DBOCAContext()
+           : base("name=DBOCAContext")
+        {
+        }
+        public DbSet<ControlMatriculas> ControlMatriculas { get; set; }
+        public DbSet<LineaVh> LineaVh { get; set; }
+        public DbSet<OrdenCompra> OrdenCompra { get; set; }
+        public DbSet<GestionOC> GestionOC { get; set; }
+        public DbSet<CarteraOC> CarteraOC { get; set; }
+        public DbSet<GestionCartera> GestionCartera { get; set; }
+        public DbSet<EjecucionOC> EjecucionOC { get; set; }
+        public DbSet<ArchivoOC> ArchivoOCs { get; set; }
+        public DbSet<TercerosOCA> TercerosOCAs { get; set; }
+        public DbSet<ArchivoCarteraOC> ArchivoCarteraOCs { get; set; }
+        public DbSet<ArchivoEjecucionOC> ArchivoEjecucionOCs { get; set; }
+        public DbSet<Proveedor> Proveedors { get; set; }
+        public DbSet<ReferenciasOC> ReferenciasOC { get; set; }
+        public DbSet<EntidadMatricula> EntidadMatriculas { get; set; }
+        public DbSet<ProveedorOC> ProveedorOCs { get; set; }
+        public DbSet<ArchivoProveedorOC> ArchivoProveedorOCs { get; set; }
+        public DbSet<Evento> Eventoes { get; set; }
+        public DbSet<Segmento> Segmentoes { get; set; }
+        public DbSet<Cilindraje> Cilindrajes { get; set; }
+        public DbSet<Adecuacion> Adecuacions { get; set; }
+        public DbSet<IntervaloPrecio> IntervaloPrecios { get; set; }
+        public DbSet<Transmision> Transmisiones { get; set; }
+        public DbSet<MttoPreventivo> MttoPreventivos { get; set; }
+        public DbSet<VigenciaSoat> VigenciaSoats { get; set; }
+        public DbSet<CiudadOCA> CiudadOCA { get; set; }        
+    }
+}
